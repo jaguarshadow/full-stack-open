ca71006b2ce78ad1f1af65dc0b765b2ac84664fc
jaguarshadow <starbucknakari@gmail.com>
2023-07-09 23:19:00 -0500

    Add assignment 1.5

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,64 @@
+
+const Header = (props) => {
+  console.log(props)
+  return ( 
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return(
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  let total = 0
+  props.parts.forEach(part => {total = total + part.exercises})
+  return(
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundementals of react',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App;
